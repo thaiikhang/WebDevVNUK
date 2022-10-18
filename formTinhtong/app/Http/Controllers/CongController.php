338da767b0256076b2c93d3f36565ab8819d4d53
Khaug <huynhkhang690@gmail.com>
2022-10-18 18:14:27 +0700

    delete function cong

--- formTinhtong/app/Http/Controllers/CongController.php
+++ formTinhtong/app/Http/Controllers/CongController.php
@@ -6,10 +6,6 @@ use Illuminate\Http\Request;
 
 class CongController extends Controller
 {
-    public function cong($a,$b){
-        return $a + $b;
-    }
-
     public function tinhtong(Request $request){
         $sum = $request->soA + $request->soB;
         return view('sum',compact('sum'));
