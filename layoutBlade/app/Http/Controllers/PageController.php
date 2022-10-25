0e41fe5b9c6814e2ab222403ab46f0b149d497ef
Khaug <huynhkhang690@gmail.com>
2022-10-25 12:21:26 +0700

    cut layout Blade template p2

--- layoutBlade/app/Http/Controllers/PageController.php
+++ layoutBlade/app/Http/Controllers/PageController.php
@@ -9,4 +9,20 @@ class PageController extends Controller
     public function getIndex(){
         return view('page.trangchu');
     }
+
+    public function getLoaiSp(){
+        return view('page.loai_sanpham');
+    }
+
+    public function getChitiet(){
+        return view('page.chitiet_sanpham');
+    }
+
+    public function getLienhe(){
+        return view('page.lienhe');
+    }
+
+    public function getAbout(){
+        return view('page.about');
+    }
 }
--- layoutBlade/routes/web.php
+++ layoutBlade/routes/web.php
@@ -19,3 +19,7 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('index',[PageController::class, 'getIndex']);
+Route::get('loai-san-pham',[PageController::class, 'getLoaiSp']);
+Route::get('chi-tiet-san-pham',[PageController::class, 'getChitiet']);
+Route::get('lien-he', [PageController::class, 'getLienhe']);
+Route::get('gioi-thieu',[PageController::class, 'getAbout']);
